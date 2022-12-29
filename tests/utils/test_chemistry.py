ac7bcfaa1699ca1e69e65061a6329b29db91586c
Logan Ward <ward.logan.t@gmail.com>
2022-12-29 10:04:13 -0500

    Add a test for the parser

--- tests/utils/test_chemistry.py
+++ tests/utils/test_chemistry.py
@@ -1,9 +1,21 @@
 """Tests for the "chemistry" utilities"""
 
-from examol.utils.chemistry import get_baseline_charge
+from rdkit import Chem
+
+from examol.utils import chemistry as chem
+
+
+def test_parse():
+    mol = chem.parse_from_molecule_string('O')
+    inchi = Chem.MolToInchi(mol)
+    assert inchi == 'InChI=1S/H2O/h1H2'
+
+    mol = chem.parse_from_molecule_string(inchi)
+    smiles = Chem.MolToSmiles(mol)
+    assert smiles == 'O'
 
 
 def test_charge():
-    assert get_baseline_charge('O') == 0
-    assert get_baseline_charge('[NH4+]') == 1
-    assert get_baseline_charge('Fc1c(F)c1=[F+]') == 1
+    assert chem.get_baseline_charge('O') == 0
+    assert chem.get_baseline_charge('[NH4+]') == 1
+    assert chem.get_baseline_charge('Fc1c(F)c1=[F+]') == 1
