abc766db7c3dde65f72fe66f95617cea8c1ae3d9
Logan Ward <ward.logan.t@gmail.com>
2022-12-30 01:27:19 +0000

    Fixes for Polaris
    1. Make a localhost file to ranks don't interfereA
    2. Skip SMILES that don't parse
    3. Update the environment path

--- scripts/redoxmers/check-chemistry-settings/run_many.py
+++ scripts/redoxmers/check-chemistry-settings/run_many.py
@@ -31,10 +31,14 @@ if __name__ == "__main__":
     to_run = pd.read_json('data/g4mp2-mols.json', lines=True)
 
     # Make the ASE calculator
+    #  Call with a local host file defined by the worker's rank
+    #  See: https://docs.alcf.anl.gov/polaris/queueing-and-running-jobs/example-job-scripts/#multi-node-ensemble-calculations-example
+    cwd = Path().cwd().absolute()
     sim = ASESimulator(
-        scratch_dir='.',
+        scratch_dir='cp2k-files',
         ase_db_path='data.db',
         cp2k_command=f'mpiexec -n {args.nodes_per_cp2k * 4} --ppn 4 --cpu-bind depth --depth 8 -env OMP_NUM_THREADS=8 '
+                     f'--hostfile {cwd}/local_hostfile.`printf %02d $((PARSL_WORKER_RANK+1))` '
                      '/lus/grand/projects/CSC249ADCD08/cp2k/set_affinity_gpu_polaris.sh '
                      '/lus/grand/projects/CSC249ADCD08/cp2k/cp2k-git/exe/local_cuda/cp2k_shell.psmp',
     )
@@ -63,16 +67,17 @@ module load conda
 module load cudatoolkit-standalone/11.4.4
 module load cray-libsci cray-fftw
 module list
-cd {os.getcwd()}
+cd {cwd}
 hostname
 pwd
-conda activate /lus/grand/projects/CSC249ADCD08/quantum-chemistry-on-polaris/env""",
+split --lines={args.nodes_per_cp2k} --numeric-suffixes=1 --suffix-length=2 $PBS_NODEFILE local_hostfile.
+conda activate /lus/grand/projects/CSC249ADCD08/ExaMol/env""",
                     walltime="1:00:00",
-                    queue="debug-scaling",
+                    queue="debug",
                     scheduler_options="#PBS -l filesystems=home:eagle:grand",
                     launcher=SimpleLauncher(),
                     select_options="ngpus=4",
-                    nodes_per_block=args.num_nodes,
+                    nodes_per_block=args.num_parallel * args.nodes_per_cp2k,
                     min_blocks=0,
                     max_blocks=1,
                     cpus_per_node=64,
@@ -88,14 +93,16 @@ conda activate /lus/grand/projects/CSC249ADCD08/quantum-chemistry-on-polaris/env
     # Loop over all of them
     futures = []
     for _, row in to_run.iterrows():
-        inchi, xyz = generate_inchi_and_xyz(row['smiles'])
+        try:
+            _, xyz = generate_inchi_and_xyz(row['smiles'])
+        except ValueError:
+            xyz = row['xyz']
         for charge in [-1, 0, 1]:
             if (row["smiles"], charge, config_name) in already_ran:
                 continue
             future = app(xyz, charge=charge, config_name=config_name)
             future.info = {'filename': row['filename'], 'smiles': row['smiles'], 'charge': charge}
             futures.append(future)
-        break
 
     # Write them out as they complete
     for future in tqdm(as_completed(futures), total=len(futures)):
