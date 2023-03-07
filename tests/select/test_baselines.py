03e7fcbaf343890fa410525ebb97b8f81fd6ea84
Logan Ward <ward.logan.t@gmail.com>
2023-03-07 14:41:04 -0500

    Fix the random selector test

--- tests/select/test_baselines.py
+++ tests/select/test_baselines.py
@@ -6,7 +6,7 @@ from examol.select.random import RandomSelector
 
 def test_random():
     selector = RandomSelector(to_select=2)
-    selector.add_possibilities(keys=[1, 2, 3], samples=np.array([1, 2, 3]))
+    selector.add_possibilities(keys=[1, 2, 3], samples=np.array([[1, 2, 3]]).T)
     selector.start_dispensing()
     assert len(list(selector.dispense())) == 2
     assert len(next(selector.dispense())) == 2
