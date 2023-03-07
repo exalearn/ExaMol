4946b14ead5bec1b39c54db053ca2b4d48bf39a3
Logan Ward <ward.logan.t@gmail.com>
2023-03-07 09:57:37 -0500

    Finish documentation

--- examol/select/base.py
+++ examol/select/base.py
@@ -12,9 +12,13 @@ class Selector:
     Selectors function in two phases: a gathering and a dispensing.
 
     The gathering phase starts by calling :meth:`start_gathering` before adding new options for computations
-    with the :meth:`add_possibilities` option.
+    with the :meth:`add_possibilities` option. ``add_possibilities`` takes a list of keys describing the computations
+    and a distribution of possible scores (e.g., predictions from different models in an ensemble) for each computation.
 
-    The dispensing phase starts after :meth:`start_dispensing` is called, which makes it
+    The dispensing phase starts after :meth:`start_dispensing` is called, which makes it then possible to pull
+    a list of prioritized computations from :meth:`dispense`. ``dispense`` generates a selected computation from
+    the list of keys acquired during gathering phase paired with a score. Selections are generated from highest
+    to lowest priority.
     """
 
     def __init__(self, to_select: int):
@@ -36,9 +40,11 @@ class Selector:
 
         Args:
             keys: Labels by which to identify the compositions being selected between
-            samples: Samples of the potential property values
+            samples: A distribution of scores for each record. For example,
+                these could be predictions of its properties from a
         """
         assert self.gathering, 'Not in gathering phase. Call `start_gathering` first'
+        assert len(keys) == len(samples), 'The list of keys and samples should be the same length'
         self._add_possibilities(keys, samples, **kwargs)
 
     def _add_possibilities(self, keys: list, samples: np.ndarray, **kwargs):
