fededd6939c77271934239ed3120659b5af6f661
Logan Ward <ward.logan.t@gmail.com>
2023-03-08 14:42:08 -0500

    Add dry run capability to CLI

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,8 +1,19 @@
+import logging
+from pathlib import Path
+
 from examol.cli import main
 from examol import __version__
+
+_spec_dir = (Path(__file__).parent / '../../examples/redoxmers/').resolve()
 
 
 def test_version(capsys):
     main(['--version'])
     cap = capsys.readouterr()
     assert __version__ in cap.out
+
+
+def test_dryrun(caplog, capsys):
+    with caplog.at_level(logging.INFO):
+        main(['run', '--dry-run', f'{_spec_dir / "spec.py"}:spec'])
+    assert 'dry run' in caplog.messages[-1]
