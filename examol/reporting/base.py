fa5112dfa09f8cb1c040ff0f6f0b78d0fa63cf8a
Logan Ward <ward.logan.t@gmail.com>
2023-03-23 12:27:13 -0400

    Add a monitor thread operation

--- examol/reporting/base.py
+++ examol/reporting/base.py
@@ -1,9 +1,35 @@
 """Base class for reporting implementations"""
+from threading import Thread
+from time import sleep
+
 from examol.steer.base import MoleculeThinker
 
 
 class BaseReporter:
     """Base class for all reporter functions"""
+
+    def monitor(self, thinker: MoleculeThinker, frequency: float = 10) -> Thread:
+        """Continually report on the progress of the thinker in a separate thread
+
+        The thread runs until the thinker halts and is launched as a daemon thread
+        so that it will not prevent the simulation from exiting properly.
+
+        Args:
+            thinker: Thinker to be monitored
+            frequency: How many seconds between report
+        Returns:
+            Thread that holds the monitor
+        """
+
+        # Make the function to be run
+        def _to_run():
+            while not thinker.done.is_set():
+                sleep(frequency)
+                self.report(thinker)
+
+        thread = Thread(target=_to_run, daemon=True, name=f'monitor_{self.__class__.__name__.lower()}')
+        thread.start()
+        return thread
 
     def report(self, thinker: MoleculeThinker):
         """Generate a report for the status of the thinker
--- tests/report/test_reporting.py
+++ tests/report/test_reporting.py
@@ -1,7 +1,9 @@
 import shutil
+from time import sleep
 from pathlib import Path
+from threading import Event
 
-from pytest import fixture
+from pytest import fixture, mark
 
 from examol.reporting.markdown import MarkdownReporter
 from examol.steer.single import SingleObjectiveThinker
@@ -19,15 +21,36 @@ class FakeThinker(SingleObjectiveThinker):
 
 
 @fixture()
-def thinker():
-    return FakeThinker()
-
-
-def test_markdown(thinker, tmpdir):
+def thinker(tmpdir):
+    thinker = FakeThinker()
     run_dir = Path(tmpdir) / 'test-dir'
     shutil.copytree(example_dir, run_dir)
     thinker.run_dir = run_dir
+    thinker.done = Event()
+    return thinker
+
+
+def test_markdown(thinker):
     reporter = MarkdownReporter()
     reporter.report(thinker)
     assert (thinker.run_dir / 'report.md').is_file()
     assert (thinker.run_dir / 'simulation-outputs.png').is_file()
+
+
+@mark.timeout(25)
+def test_monitor(thinker):
+    reporter = MarkdownReporter()
+    thread = reporter.monitor(thinker, frequency=1)
+    thread.join(1)
+    assert not thinker.done.is_set()
+    assert thread.is_alive()
+
+    # Make sure it writes after at least 2s
+    sleep(2)
+    assert thread.is_alive(), thread.join()
+    assert (thinker.run_dir / 'report.md').is_file()
+
+    # Make sure it shuts down
+    thinker.done.set()
+    sleep(2)
+    assert not thread.is_alive()
