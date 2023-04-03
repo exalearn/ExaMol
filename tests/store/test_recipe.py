6cf6e43738fd1d7ddd158e346f70803531992f80
Logan Ward <WardLT@users.noreply.github.com>
2023-04-03 16:20:50 -0400

    Fix some errors in suggesting redoxmer computations (#32)
    * Found first bug: running too many single-point
    
    * Fixed the first bug
    
    * Caught a bug in suggesting computations
    
    * Switch away from assert for user feedback

--- tests/store/test_recipe.py
+++ tests/store/test_recipe.py
@@ -49,7 +49,7 @@ def test_solvation(record, sim_result):
     assert len(requests) == 0
 
 
-def test_redox(record, sim_result):
+def test_redox_vacuum(record, sim_result):
     recipe = RedoxEnergy(1, 'test', vertical=True)
     requests = recipe.suggest_computations(record)
     assert len(requests) == 1
@@ -79,7 +79,7 @@ def test_redox(record, sim_result):
     recipe = RedoxEnergy(1, 'test', vertical=False)
     assert 'adiabatic' in recipe.level
     assert 'oxid' in recipe.name
-    with raises(AssertionError) as error:
+    with raises(ValueError) as error:
         recipe.compute_property(record)
     assert 'We do not have a relaxed charged molecule' in str(error)
 
@@ -100,30 +100,110 @@ def test_redox(record, sim_result):
     requests = recipe.suggest_computations(record)
     assert len(requests) == 0
 
-    # Start assessing the energy in solvent
+
+def test_redox_solvent_vertical(record, sim_result):
+    recipe = RedoxEnergy(1, 'test', solvent='acn', vertical=True)
+
+    # For vertical, we only need one relaxation to start
+    requests = recipe.suggest_computations(record)
+    assert len(requests) == 1
+    assert requests[0].optimize
+    assert requests[0].config_name == 'test'
+    assert requests[0].charge == 0
+
+    # Once we add that, we will need 2 computations (neutral solvent, charged solvent)
+    record.add_energies(sim_result)
+
+    requests = recipe.suggest_computations(record)
+    assert len(requests) == 2, requests
+    assert not any(r.optimize for r in requests)
+    assert all(r.config_name == 'test' for r in requests)
+    assert set(r.charge for r in requests) == {0, 1}
+
+    # Make sure that we cannot yet compute the property
+    with raises(ValueError):
+        recipe.compute_property(record)
+
+    # Add neutral energy in solvent
+    sim_result.solvent = 'acn'
+    sim_result.energy -= 0.5
+    record.add_energies(sim_result)
+    with raises(ValueError):
+        recipe.compute_property(record)
+    assert len(recipe.suggest_computations(record)) == 1
+
+    # Add charged energy in solvent
+    sim_result.charge = recipe.charge
+    sim_result.energy += 1
+    record.add_energies(sim_result)
+    assert recipe.compute_property(record) == 1
+    assert recipe.suggest_computations(record) == []
+
+
+def test_redox_solvent_adiabatic(record, sim_result):
     recipe = RedoxEnergy(1, 'test', solvent='acn', vertical=False)
 
+    # For adiabatic, we only need a relaxation to start
     requests = recipe.suggest_computations(record)
     assert len(requests) == 2
-    assert not any(x.optimize for x in requests)
-    assert any(x.solvent == 'acn' for x in requests)
-    assert requests[0].charge == 0 and requests[0].xyz == sim_result.xyz
-    assert requests[1].charge == 1 and requests[1].xyz == adia_sim_result.xyz
+    assert all(r.optimize for r in requests)
+    assert all(r.xyz != sim_result.xyz for r in requests)
+    assert all(r.config_name == 'test' for r in requests)
+    assert set(r.charge for r in requests) == {0, 1}
 
-    adia_sim_result.solvent = 'acn'
-    adia_sim_result.energy -= 2
-    assert not record.add_energies(adia_sim_result)
+    # If we add the relaxed neutral, start the charged from that geometry instead
+    record.add_energies(sim_result)
+    requests = recipe.suggest_computations(record)
 
-    sim_result.charge = 0
+    assert len(requests) == 2
+
+    assert not requests[0].optimize
+    assert requests[0].charge == 0
+    assert requests[0].solvent == 'acn'
+
+    assert requests[1].optimize
+    assert requests[1].xyz == sim_result.xyz
+    assert requests[1].charge == 1
+    assert requests[1].solvent is None
+
+    # Add the neutral in solvent
+    adia_sim_result = copy.copy(sim_result)  # Copy before altering
     sim_result.solvent = 'acn'
-    sim_result.energy -= 2
-    assert not record.add_energies(sim_result)
+    sim_result.energy -= 1
+    record.add_energies(sim_result)
 
-    assert 'acn' in recipe.level
-    assert isclose(recipe.compute_property(record), -0.5)
+    with raises(ValueError):
+        recipe.compute_property(record)
 
     requests = recipe.suggest_computations(record)
-    assert len(requests) == 0
+
+    assert len(requests) == 1
+
+    # Add the charged relaxed. We should now request a single-point, charged, in solution
+    adia_sim_result.solvent = None
+    adia_sim_result.charge = 1
+    adia_sim_result.xyz = sim_result.xyz.replace('0.000', '0.010')
+    adia_sim_result.energy += 1
+    record.add_energies(adia_sim_result)
+
+    with raises(ValueError):
+        recipe.compute_property(record)
+
+    requests = recipe.suggest_computations(record)
+
+    assert len(requests) == 1
+
+    assert not requests[0].optimize
+    assert requests[0].xyz == adia_sim_result.xyz
+    assert requests[0].charge == 1
+    assert requests[0].solvent == 'acn'
+
+    # Add that, make sure we compute correctly
+    adia_sim_result.solvent = 'acn'
+    adia_sim_result.energy -= 1.75
+    record.add_energies(adia_sim_result)
+    assert recipe.compute_property(record) == 0.25
+    assert recipe.suggest_computations(record) == []
 
 
 def test_adia_redox(record):
