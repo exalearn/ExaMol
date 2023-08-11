33b01ed48f4eb3153df8be7d680b01aa7ec64611
mbae26 <minseok@stanford.edu>
2023-08-11 09:46:35 -0500

    feat: implement kmeans.py for kmeans starter to select molecules for training and write test_kmeans.py to verify its functionality and correctness.

--- examol/start/kmeans.py
+++ examol/start/kmeans.py
@@ -1,16 +1,42 @@
 """Algorithms which select molecules based on k-means clustering"""
+import numpy as np
+from sklearn.cluster import KMeans
 from .base import Starter
 from examol.score.rdkit.descriptors import compute_morgan_fingerprints
+from scipy.spatial.distance import cdist
 
 
 class KMeansStarter(Starter):
     """Select structurally distinct molecules by picking molecules at the centers of clusters"""
 
     def _select(self, to_select: list[str], count: int) -> list[str]:
-        # Compute the fingerprints for each molecule
-        finger = [
-            compute_morgan_fingerprints(x) for x in to_select
-        ]
-
-        # Pick the molecules at the center of each cluster
-        raise NotImplementedError()
+        """
+        Input:
+            to_select: list of SMILES strings to select from
+            count: number of molecules to select
+        Output:
+            list of SMILES strings of selected molecules
+        """
+        if count > len(to_select):
+            raise ValueError(f"Cannot select {count} molecules from {len(to_select)} molecules.")
+        
+        # Compute Morgan fingerprints along with their indices
+        fingerprints = [compute_morgan_fingerprints(smiles) for smiles in to_select]
+        
+        # Run KMeans clustering on the fingerprints
+        kmeans = KMeans(n_clusters=count).fit(fingerprints)
+        
+        # Get cluster centers
+        cluster_centers = kmeans.cluster_centers_
+        
+        # Get the molecules closest to the cluster centers
+        # Using cdist to compute the distance between each pair of fingerprints and cluster centers
+        distances = cdist(fingerprints, cluster_centers, 'euclidean')
+        
+        closest_molecules = []
+        # For each cluster center, find the molecule with the smallest distance to it
+        for i in range(count):
+            closest_index = np.argmin(distances[:, i])
+            closest_molecules.append(to_select[closest_index])
+        
+        return closest_molecules
