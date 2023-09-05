a6f3dfbf1943d1e640d91e0f72d03a4e1996eb8a
Logan Ward <ward.logan.t@gmail.com>
2023-09-05 09:28:22 -0400

    Switch to using Colmena
    Should enable better scaling

--- scripts/redoxmers/2_initial-data/0_gather-initial-dataset.py
+++ scripts/redoxmers/2_initial-data/0_gather-initial-dataset.py
@@ -1,21 +1,161 @@
 """Compute an initial dataset for various recipes"""
-from concurrent.futures import Future, wait, ALL_COMPLETED
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from collections import defaultdict
+from threading import Event
+from queue import LifoQueue
 from pathlib import Path
+from shutil import move
 import logging
 import sys
 
+from colmena.models import Result
 from rdkit import RDLogger
-from parsl.app.python import PythonApp
-import parsl
+from proxystore.connectors.file import FileConnector
+from proxystore.store import Store, register_store
+from colmena.thinker import BaseThinker, ResourceCounter, event_responder, task_submitter, result_processor
+from colmena.task_server.parsl import ParslTaskServer
+from colmena.queue import PipeQueues
 
-from examol.simulate.base import SimResult
 from examol.store.models import MoleculeRecord
 from examol.store.recipes import RedoxEnergy
 
 import configs
 
 RDLogger.DisableLog('rdApp.*')
+
+
+# Make the steering application
+class BruteForceThinker(BaseThinker):
+    """Evaluate all molecules in a user-provided list
+
+    Args:
+        queues: Queues used to communicate with task server
+        args: Arguments provided to the run script
+        slots: Number of molecules to run concurrently
+        database: All known information about each molecule
+        recipes: List of recipes to be computed
+        database_path: Path in which to store the database
+        optimization_path: Path in which to store optimization steps
+    """
+
+    def __init__(self,
+                 queues: PipeQueues,
+                 args: Namespace,
+                 slots: int,
+                 database: dict[str, MoleculeRecord],
+                 recipes: list[RedoxEnergy],
+                 database_path: Path,
+                 optimization_path: Path | None):
+        super().__init__(queues, ResourceCounter(slots))
+        self.optimization_path = optimization_path
+        self.database_path = database_path
+        self.database = database
+        self.recipes = recipes
+        self.args = args
+
+        # Determine where to store the task records
+        run_name = self.database_path.name[:-5]
+        self.record_path = self.database_path.parent / f'{run_name}-results.json'
+
+        # State tracking
+        self.molecule_queue: LifoQueue[str] = LifoQueue()  # List of molecules which are yet to be computed
+        self.ongoing_tasks: dict[str, int] = defaultdict(int)  # How many tasks ongoing for each molecule
+        self.checkpoint = Event()
+
+        # Populate the list of molecules to run
+        for key, _ in zip(database.keys(), range(len(self.database) if args.num_to_run is None else args.num_to_run)):
+            self.molecule_queue.put(key)
+
+    @task_submitter()
+    def submit_task(self):
+        """Submit tasks for a new molecule"""
+
+        # Loop until we either find new work or run out of molecules
+        while not self.molecule_queue.empty():
+            # Get the next record
+            key = self.molecule_queue.get()
+            my_record = self.database[key]
+
+            # See if there is any new work to do
+            for recipe in self.recipes:
+                try:
+                    next_calculations = recipe.suggest_computations(my_record)
+                    if len(next_calculations) > 0:
+                        my_logger.debug(f'Submitting tasks for {my_record.key} recipe {recipe.name}@{recipe.level}')
+                        # Submit them
+                        for request in next_calculations:
+                            method = 'optimize_structure' if request.optimize else 'compute_energy'
+                            self.queues.send_inputs(
+                                my_record.key, request.xyz, request.config_name, request.charge, request.solvent,
+                                method=method,
+                                task_info={'key': my_record.key, 'recipe': recipe.name, 'level': recipe.level}
+                            )
+                            self.ongoing_tasks[my_record.key] += 1
+
+                        # Finished!
+                        return
+
+                    # Compute the property
+                    recipe.update_record(my_record)
+                except ValueError as e:
+                    logger.warning(f'{my_record.key} failed for {recipe.name}@{recipe.level}. Error: {e}')
+                    if self.args.halt_on_error:
+                        raise ValueError(f'Failed to submit new tasks for {my_record.key}')
+
+        # If there are neither molecules no ongoing tasks, then we are done
+        if len(self.ongoing_tasks) == 0:
+            self.done.set()
+
+    @result_processor()
+    def store_result(self, result: Result):
+        """Store the result from a computation"""
+
+        # Get the associated record
+        my_record = self.database[result.task_info['key']]
+        self.ongoing_tasks[my_record.key] -= 1
+
+        # Save the result to disk
+        with self.record_path.open('a') as fp:
+            print(result.json(exclude={'value', 'inputs'}), file=fp)
+
+        # Update the record
+        if not result.success:
+            self.logger.warning(f'Computation failed for {my_record.key}. Error: {result.failure_info}')
+            if args.halt_on_error:
+                raise ValueError(f'Computation failed. Halting everything')
+        else:
+            if result.method == 'optimize_structure':
+                sim_result, steps, metadata = result.value
+                my_record.add_energies(sim_result, steps)
+
+                # Save the steps to disk
+                if self.optimization_path is not None:
+                    with self.optimization_path.open('a') as fp:
+                        for step in steps:
+                            print(step.json(), file=fp)
+            else:
+                sim_result, metadata = result.value
+                my_record.add_energies(sim_result)
+
+        # Check if we need more work for this molecule
+        self.logger.debug(f'Stored record for {my_record.key}. {self.ongoing_tasks[my_record.key]} tasks remaining')
+        if self.ongoing_tasks[my_record.key] == 0:
+            self.molecule_queue.put(my_record.key)
+            self.ongoing_tasks.pop(my_record.key)
+            self.rec.release()
+
+        # Trigger the engine to checkpoint our database
+        self.checkpoint.set()
+
+    @event_responder(event_name='checkpoint')
+    def write_database(self):
+        """Save the database to disk"""
+        temp_path = self.database_path.parent / f'{self.database_path.name}.new'
+        with temp_path.open('w') as fp:
+            for record in dataset.values():
+                print(record.to_json(), file=fp)
+        move(temp_path, self.database_path)
+
 
 if __name__ == "__main__":
     # Make the argument parser
@@ -69,9 +209,9 @@ if __name__ == "__main__":
                 print(record.to_json(), file=fp)
     my_logger.info(f'Starting from a dataset of {len(dataset)} records')
 
-    #  Get the right computational environment
+    # Get the right computational environment
     config_fn = getattr(configs, args.config_function)
-    config, sim, energy_configs = config_fn()
+    config, sim, n_slots, energy_configs = config_fn()
     my_logger.info(f'Loaded configuration function "{args.config_function}"')
 
     # Get the recipes we should run
@@ -85,116 +225,37 @@ if __name__ == "__main__":
                 ])
     my_logger.info(f'Assembled a list of {len(recipes)} recipes to compute')
 
-    # Start Parsl and wrap the workflow functions
-    parsl.load(config)
-    energy_app = PythonApp(sim.compute_energy)
-    relax_app = PythonApp(sim.optimize_structure)
-    my_logger.info(f'Loaded Parsl and build applications')
+    # Create the queues which will connect task server and thinker
+    store = Store(name='file', connector=FileConnector(store_dir='proxy-store'))
+    register_store(store)
+    queues = PipeQueues(proxystore_name='file', proxystore_threshold=10000)
 
-    # Submit an initial batch of work
-    ongoing_tasks: dict[str, int] = dict((x, 0) for x in dataset)  # How many tasks ongoing for each molecule
-    futures: list[Future] = []
+    # Create the task server
+    task_server = ParslTaskServer(queues=queues, methods=[sim.compute_energy, sim.optimize_structure], config=config)
+    my_logger.info('Created task server')
 
+    # Create the thinker
+    thinker = BruteForceThinker(queues, args, n_slots, dataset, recipes, dataset_path, records_path)
+    thinker.logger.addHandler(handler)
+    thinker.logger.setLevel(logging.INFO)
 
-    def submit_new_work(my_record: MoleculeRecord) -> list[Future]:
-        """Submit all the new tasks for a record
+    # Run the script
+    try:
+        task_server.start()
+        thinker.run()
+    finally:
+        queues.send_kill_signal()
 
-        Args:
-            my_record: Record to be evaluated
+    task_server.join()
 
-        Returns:
-            The futures produced by the calculations,
-            with the attribute ".key" of each set as the key for the record
-            and the attribute of ".optimize" set to whether this is an optimization or not
-        """
+    # Save the result one last time
+    temp_path = dataset_path.parent / f'{dataset_path.name}.new'
+    with temp_path.open('w') as fp:
+        for record in dataset.values():
+            print(record.to_json(), file=fp)
+    move(temp_path, dataset_path)
+    my_logger.info(f'Wrote database to {dataset_path}')
 
-        for recipe in recipes:
-            try:
-                next_calculations = recipe.suggest_computations(my_record)
-                if len(next_calculations) > 0:
-                    my_logger.debug(f'Submitting tasks for {my_record.key} recipe {recipe.name}@{recipe.level}')
-                    # Submit them
-                    my_futures = []
-                    for request in next_calculations:
-                        app = relax_app if request.optimize else energy_app
-                        my_future = app(my_record.key, request.xyz, request.config_name, request.charge, request.solvent)
-                        my_future.key = my_record.key
-                        my_future.optimize = request.optimize
-                        my_futures.append(my_future)
-
-                    # Update the ongoing task list
-                    ongoing_tasks[my_record.key] += len(my_futures)
-                    return my_futures
-
-                # Compute the property
-                recipe.update_record(my_record)
-            except ValueError as e:
-                logger.warning(f'{my_record.key} failed for {recipe.name}@{recipe.level}. Error: {e}')
-                if args.halt_on_error:
-                    raise
-                return []
-
-        my_logger.debug(f'Done with {len(recipes)} recipes for {my_record.key}')
-        return []
-
-
-    for record, _ in zip(dataset.values(), range(len(dataset) if args.num_to_run is None else args.num_to_run)):
-        new_futures = submit_new_work(record)
-        futures.extend(new_futures)
-
-    my_logger.info(f'Started an initial batch of {len(futures)} calculations')
-
-    # Store and submit new records as they complete
-    while len(futures) > 0:
-        # Wait until all completed or 5s
-        done, futures = wait(futures, timeout=args.write_frequency, return_when=ALL_COMPLETED)
-
-        # Process completed records
-        if len(done) == 0:
-            continue
-        new_futures = []
-        simulation_records: list[SimResult] = []
-        my_logger.info(f'{len(done)} tasks have completed')
-        for future in done:
-            # Get the associated record
-            record = dataset[future.key]
-
-            # Update the record
-            try:
-                result = future.result()
-            except BaseException as e:
-                my_logger.warning(f'Computation failed for {record.key}. Error: {e}')
-                if args.halt_on_error:
-                    raise
-                continue
-            if future.optimize:
-                sim_result, steps, metadata = result
-                simulation_records.extend([sim_result] + steps)
-                record.add_energies(sim_result, steps)
-            else:
-                sim_result, metadata = result
-                simulation_records.append(sim_result)
-                record.add_energies(sim_result)
-
-            # Check if we need more work for this molecule
-            ongoing_tasks[record.key] -= 1
-            my_logger.debug(f'Stored record for {record.key}. {ongoing_tasks[record.key]} tasks remaining')
-            if ongoing_tasks[record.key] == 0:
-                my_logger.debug(f'Submitting new work for {record.key}')
-                new_futures.extend(submit_new_work(record))
-
-        futures.update(new_futures)
-        my_logger.info(f'Submitted {len(new_futures)} calculations. Total ongoing tasks is {len(futures)}')
-
-        # Save the database
-        with dataset_path.open('w') as fp:
-            for record in dataset.values():
-                print(record.to_json(), file=fp)
-        my_logger.info(f'Wrote updated dataset to {dataset_path}')
-
-        # Write the energy/forces to disk
-        #  TODO (wardlt): Write to a temporary path and then copy
-        with records_path.open('a') as fp:
-            for record in simulation_records:
-                print(record.json(), file=fp)
-        my_logger.info(f'Appended {len(simulation_records)} new simulation records to {records_path}')
+    # Close the proxystore
+    store.close()
+    my_logger.info('Done!')
--- scripts/redoxmers/2_initial-data/configs.py
+++ scripts/redoxmers/2_initial-data/configs.py
@@ -2,6 +2,7 @@
 
 Add a new environment by creating a function which produces a Simulation tool,
 Parsl config for the desired environment,
+the number of execution slots,
 and a list of the energy configurations it supports
 """
 from parsl.executors import HighThroughputExecutor
@@ -13,17 +14,17 @@ from parsl.config import Config
 from examol.simulate.ase import ASESimulator
 
 
-def make_local_config() -> tuple[Config, ASESimulator, list[str]]:
+def make_local_config() -> tuple[Config, ASESimulator, int, list[str]]:
     """Make a configuration that will run computations locally"""
 
     config = Config(
         executors=[HighThroughputExecutor(max_workers=1, address='127.0.0.1')]
     )
     sim = ASESimulator(scratch_dir='run_data')
-    return config, sim, ['xtb', 'mopac_pm7']
+    return config, sim, 1, ['xtb', 'mopac_pm7']
 
 
-def make_theta_config() -> tuple[Config, ASESimulator, list[str]]:
+def make_theta_config() -> tuple[Config, ASESimulator, int, list[str]]:
     """Make a configuration that will run computations locally"""
 
     config = Config(
@@ -33,6 +34,7 @@ def make_theta_config() -> tuple[Config, ASESimulator, list[str]]:
                 max_workers=16,
                 cpu_affinity='block',
                 address=address_by_interface('vlan2360'),
+                prefetch_capacity=16,
                 provider=CobaltProvider(
                     queue='default',
                     account='CSC249ADCD08',
@@ -47,4 +49,4 @@ def make_theta_config() -> tuple[Config, ASESimulator, list[str]]:
             )]
     )
     sim = ASESimulator(scratch_dir='run_data', retain_failed=False)  # Reduce the disk footprint
-    return config, sim, ['xtb', 'mopac_pm7']
+    return config, sim, 128 * 16 * 2, ['xtb', 'mopac_pm7']
