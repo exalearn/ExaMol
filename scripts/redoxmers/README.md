6747eafb7f410fe4aa022b6ae3ff2d376ac72a6d
Logan Ward <WardLT@users.noreply.github.com>
2023-11-16 11:54:15 -0500

    Readying for ExaRedox (#121)
    * Remove the scripts
    
    Now in https://github.com/exalearn/examol-redoxmers
    
    * Switch to the main ase
    
    The PRs I needed have been merged
    
    * Use main for Mac OSX too
    
    * Add operation for storing initial geometry in record
    
    Will be useful as we use Kelvin's MPNN models
    
    * Skip bad conformers

--- scripts/redoxmers/README.md
+++
@@ -1,5 +0,0 @@
-# Redoxmer Design
-
-Design of molecules that store energy inside redox-flow batteries.
-
-The subfolders include the process from validating the quantum chemistry methods through running AI-guided design at scale.
--- tests/simulation/test_initialize.py
+++ tests/simulation/test_initialize.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from examol.simulate.initialize import add_initial_conformer
+from examol.store.models import MoleculeRecord, Conformer
 from examol.utils.conversions import read_from_string
 from examol.simulate import initialize as init
 
@@ -16,3 +18,37 @@ def test_geoms():
     atoms = read_from_string(xyz, 'xyz')
     for i in range(3, 6):  # For each atom attached to the ion
         assert np.linalg.det(atoms.positions[:3, :] - atoms.positions[i, :]) < 1e-6  # Test for coplanar
+
+
+def test_record():
+    # Test adding a conformer
+    record = MoleculeRecord.from_identifier('C')
+    assert len(record.conformers) == 0
+
+    assert add_initial_conformer(record) is record  # Return self
+    assert len(record.conformers) == 1
+    orig_conf = record.conformers[0]
+
+    # Add a perturbed geometry, make sure it has a higher energy
+    new_conf = Conformer.from_xyz(
+        orig_conf.xyz.replace('C      -0.0126', 'C      -0.0200'),
+        source='perturb',
+        config_name='mmff',
+        charge=0
+    )
+    record.conformers.append(new_conf)
+    add_initial_conformer(record)
+    assert all(len(x.energies) == 1 for x in [new_conf, orig_conf])
+    assert new_conf.energies[0].energy > orig_conf.energies[0].energy
+
+    # Make sure it does not add energies to charged molecules
+    charged_conf = Conformer.from_xyz(
+        orig_conf.xyz.replace('C      -0.0126', 'C      -0.0300'),
+        source='perturb',
+        config_name='mmff',
+        charge=1
+    )
+    record.conformers.append(charged_conf)
+    add_initial_conformer(record)
+    assert all(len(x.energies) == 1 for x in [new_conf, orig_conf])
+    assert len(charged_conf.energies) == 0
