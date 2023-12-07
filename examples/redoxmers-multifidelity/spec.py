5417f1d7214c9838861ff4391e608a94b74541d1
Logan Ward <ward.logan.t@gmail.com>
2023-12-07 15:48:20 -0500

    Update to multi-property, add to analysis

--- examples/redoxmers-multifidelity/spec.py
+++ examples/redoxmers-multifidelity/spec.py
@@ -9,28 +9,33 @@ from proxystore.connectors.file import FileConnector
 from proxystore.store import Store
 
 from examol.reporting.markdown import MarkdownReporter
-from examol.score.rdkit import RDKitScorer, make_gpr_model, FingerprintTransformer
+from examol.score.rdkit import RDKitScorer, make_knn_model, FingerprintTransformer
 from examol.simulate.ase import ASESimulator
 from examol.solution import MultiFidelityActiveLearning
 from examol.start.fast import RandomStarter
 from examol.steer.multifi import PipelineThinker
-from examol.store.recipes import RedoxEnergy
-from examol.select.bayes import ExpectedImprovement
+from examol.store.recipes import RedoxEnergy, SolvationEnergy
+from examol.select.botorch import EHVISelector
 from examol.specify import ExaMolSpecification
 
 # Get my path. We'll want to provide everything as absolute paths, as they are relative to this file
 my_path = Path().absolute()
 num_workers: int = 4
-multi_fidelity: bool = True
+multi_fidelity: bool = False
+random: bool = True
 colmena_queues = partial(RedisQueues)  # Using a partial in case we need to pin options, such as port numbers
 
-run_dir = my_path / ('run' if multi_fidelity else 'run-single')
+# Make a run directory named for the settings
+run_dir = my_path / (('run' if multi_fidelity else 'run-single') + ('-random' if random else ''))
 
-# Make the recipe
-recipe = RedoxEnergy(1, energy_config='mopac_pm7', solvent='acn')
+# Make the recipes to be optimized
+recipes = [
+    RedoxEnergy(1, energy_config='mopac_pm7', solvent='acn'),
+    SolvationEnergy('mopac_pm7', 'acn')
+]
 
 # Make the scorer
-pipeline = make_gpr_model()
+pipeline = make_knn_model(n_neighbors=4)
 transform: FingerprintTransformer = pipeline.steps.pop(0)[1]
 transform.n_jobs = 1
 pipeline[0].n_jobs = 1
@@ -58,23 +63,25 @@ config = Config(
 
 
 # Mark that we're going to solve this with multifidelity learning
+num_to_run = 200
 solution = MultiFidelityActiveLearning(
-    selector=ExpectedImprovement(200, maximize=True, epsilon=0.1),
+    selector=EHVISelector(200, maximize=[True, False]),
     steps=[  # The order of codes to run
         [RedoxEnergy(1, energy_config='mopac_pm7', vertical=True)],
         [RedoxEnergy(1, energy_config='mopac_pm7', vertical=True, solvent='acn')]
     ] if multi_fidelity else [],
     starter=RandomStarter(),
+    minimum_training_size=num_to_run if random else 10,
     scorer=scorer,
-    models=[[pipeline] * 8],
-    num_to_run=200,
+    models=[[pipeline] * 8] * 2,  # 8 models for 2 properties
+    num_to_run=num_to_run,
     pipeline_target=0.5,
 )
 
 # Build the specification
 spec = ExaMolSpecification(
     database=run_dir / 'database.json',
-    recipes=[recipe],
+    recipes=recipes,
     solution=solution,
     search_space=[(my_path / '..' / 'redoxmers-bebop' / 'search-space.smi')],
     simulator=sim,
