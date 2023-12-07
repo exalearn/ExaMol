1bf8858e92791a608bc0567f8770873370b0055d
Logan Ward <ward.logan.t@gmail.com>
2023-12-07 09:02:17 -0500

    Remove now-deprecated Parsl option

--- tests/steer/conftest.py
+++ tests/steer/conftest.py
@@ -74,7 +74,7 @@ def queues(recipe, scorer, simulator, tmp_path) -> ColmenaQueues:
     # Make parsl configuration
     config = Config(
         run_dir=str(tmp_path),
-        executors=[HighThroughputExecutor(start_method='spawn', max_workers=1, address='127.0.0.1')]
+        executors=[HighThroughputExecutor(max_workers=1, address='127.0.0.1')]
     )
 
     doer = ParslTaskServer(
